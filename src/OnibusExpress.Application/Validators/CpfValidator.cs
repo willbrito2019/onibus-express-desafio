731f41caebdd4ba81a30d5a9771ff5e328a9e5a7
willbrito2019 <william.diasbrito@gmail.com>
2026-07-14 12:02:54 -0300

    CpfValidator com validação de dígito verificador + testes

---
+++ src/OnibusExpress.Application/Validators/CpfValidator.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OnibusExpress.Application.Validators;
+
+public static class CpfValidator
+{
+    public static bool EhValido(string? cpf)
+    {
+        if (string.IsNullOrWhiteSpace(cpf))
+            return false;
+
+        var digitos = Regex.Replace(cpf, @"[^\d]", "");
+
+        if (digitos.Length != 11)
+            return false;
+       
+        if (digitos.Distinct().Count() == 1)
+            return false;
+
+        return DigitosVerificadoresValidos(digitos);
+    }
+
+    private static bool DigitosVerificadoresValidos(string cpf)
+    {
+        var numeros = cpf.Select(c => c - '0').ToArray();
+
+        var primeiroDigito = CalcularDigito(numeros, 9);
+        if (primeiroDigito != numeros[9])
+            return false;
+
+        var segundoDigito = CalcularDigito(numeros, 10);
+        if (segundoDigito != numeros[10])
+            return false;
+
+        return true;
+    }
+
+    private static int CalcularDigito(int[] numeros, int quantidade)
+    {
+        var soma = 0;
+        var multiplicador = quantidade + 1;
+
+        for (var i = 0; i < quantidade; i++)
+        {
+            soma += numeros[i] * multiplicador;
+            multiplicador--;
+        }
+
+        var resto = soma % 11;
+        return resto < 2 ? 0 : 11 - resto;
+    }
+}
---
+++ tests/OnibusExpress.Tests/Application/CpfValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using OnibusExpress.Application.Validators;
+using Shouldly;
+using Xunit;
+
+namespace OnibusExpress.Tests.Application;
+
+public class CpfValidatorTests
+{
+    [Theory]
+    [InlineData("11144477735")]   // CPF válido
+    [InlineData("111.444.777-35")] // formatado
+    public void Deve_Validar_Cpf_Correto(string cpf)
+    {
+        CpfValidator.EhValido(cpf).ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData("12345678900")]     // dígitos verificadores errados
+    [InlineData("11111111111")]     // todos iguais
+    [InlineData("123")]             // tamanho errado
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("abc.def.ghi-jk")]  // não numérico
+    public void Nao_Deve_Validar_Cpf_Invalido(string? cpf)
+    {
+        CpfValidator.EhValido(cpf).ShouldBeFalse();
+    }
+}
