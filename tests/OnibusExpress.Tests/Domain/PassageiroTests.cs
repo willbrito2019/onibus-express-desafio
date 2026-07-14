322d639ce755ef01997be24ecc3c091430436b46
willbrito2019 <william.diasbrito@gmail.com>
2026-07-14 11:50:51 -0300

    Demais Tests

---
+++ tests/OnibusExpress.Tests/Domain/PassageiroTests.cs
@@ -0,0 +1,40 @@
+﻿using OnibusExpress.Domain.Entities;
+using Shouldly;
+using Xunit;
+
+namespace OnibusExpress.Tests.Domain;
+
+public class PassageiroTests
+{
+    [Fact]
+    public void Deve_Criar_Passageiro_Valido()
+    {
+        var passageiro = new Passageiro(
+            "João da Silva",
+            "12345678909",
+            "joao@email.com",
+            new DateOnly(1990, 5, 10));
+
+        passageiro.Id.ShouldNotBe(Guid.Empty);
+        passageiro.Nome.ShouldBe("João da Silva");
+        passageiro.Cpf.ShouldBe("12345678909");
+        passageiro.Email.ShouldBe("joao@email.com");
+    }
+
+    [Theory]
+    [InlineData("")]    
+    public void Nao_Deve_Criar_Passageiro_Com_Nome_Vazio(string nome)
+    {
+        Should.Throw<ArgumentException>(() =>
+            new Passageiro(nome, "12345678909", "joao@email.com", new DateOnly(1990, 5, 10)));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("email-invalido")]    
+    public void Nao_Deve_Criar_Passageiro_Com_Email_Invalido(string email)
+    {
+        Should.Throw<ArgumentException>(() =>
+            new Passageiro("João da Silva", "12345678909", email, new DateOnly(1990, 5, 10)));
+    }
+}
---
+++ tests/OnibusExpress.Tests/Domain/ReservaTests.cs
@@ -0,0 +1,68 @@
+﻿using OnibusExpress.Domain.Entities;
+using Shouldly;
+using Xunit;
+
+namespace OnibusExpress.Tests.Domain;
+
+public class ReservaTests
+{
+    [Fact]
+    public void Deve_Criar_Reserva_Valida()
+    {
+        var reserva = new Reserva(Guid.NewGuid(), Guid.NewGuid(), numeroAssento: 15, codigoReserva: "ABC-12345");
+
+        reserva.Id.ShouldNotBe(Guid.Empty);
+        reserva.NumeroAssento.ShouldBe(15);
+        reserva.CodigoReserva.ShouldBe("ABC-12345");
+        reserva.Status.ShouldBe(StatusReserva.Confirmada);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Nao_Deve_Criar_Reserva_Com_Assento_Invalido(int assento)
+    {
+        Should.Throw<ArgumentException>(() =>
+            new Reserva(Guid.NewGuid(), Guid.NewGuid(), assento, "ABC-12345"));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Nao_Deve_Criar_Reserva_Com_Codigo_Vazio(string? codigo)
+    {
+        Should.Throw<ArgumentException>(() =>
+            new Reserva(Guid.NewGuid(), Guid.NewGuid(), 15, codigo!));
+    }
+
+    [Fact]
+    public void Deve_Cancelar_Reserva_Quando_Faltam_Mais_De_2_Horas_Para_Partida()
+    {
+        var reserva = new Reserva(Guid.NewGuid(), Guid.NewGuid(), 15, "ABC-12345");
+        var partida = DateTime.UtcNow.AddHours(3);
+
+        reserva.Cancelar(partida);
+
+        reserva.Status.ShouldBe(StatusReserva.Cancelada);
+    }
+
+    [Fact]
+    public void Nao_Deve_Cancelar_Reserva_Quando_Faltam_Menos_De_2_Horas_Para_Partida()
+    {
+        var reserva = new Reserva(Guid.NewGuid(), Guid.NewGuid(), 15, "ABC-12345");
+        var partida = DateTime.UtcNow.AddHours(1);
+
+        Should.Throw<InvalidOperationException>(() => reserva.Cancelar(partida));
+
+        reserva.Status.ShouldBe(StatusReserva.Confirmada); // garante que não mudou o estado
+    }
+
+    [Fact]
+    public void Nao_Deve_Cancelar_Reserva_Exatamente_No_Limite_De_2_Horas()
+    {
+        var reserva = new Reserva(Guid.NewGuid(), Guid.NewGuid(), 15, "ABC-12345");
+        var partida = DateTime.UtcNow.AddHours(2).AddSeconds(-1);
+
+        Should.Throw<InvalidOperationException>(() => reserva.Cancelar(partida));
+    }
+}
