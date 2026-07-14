9d53ab22be17d706a6f462dae8f87fa806f84999
willbrito2019 <william.diasbrito@gmail.com>
2026-07-14 12:07:19 -0300

    GeradorCodigoReserva

---
+++ src/OnibusExpress.Application/Services/GeradorCodigoReserva.cs
@@ -0,0 +1,23 @@
+﻿namespace OnibusExpress.Application.Services;
+
+public interface IGeradorCodigoReserva
+{
+    string Gerar();
+}
+
+public class GeradorCodigoReserva : IGeradorCodigoReserva
+{
+    private const string Letras = "ABCDEFGHJKLMNPQRSTUVWXYZ"; // sem I, O (confundem com 1, 0)
+    private static readonly Random Random = new();
+
+    public string Gerar()
+    {
+        var prefixo = new string(Enumerable.Range(0, 3)
+            .Select(_ => Letras[Random.Next(Letras.Length)])
+            .ToArray());
+
+        var numero = Random.Next(0, 100000).ToString("D5");
+
+        return $"{prefixo}-{numero}";
+    }
+}
---
+++ tests/OnibusExpress.Tests/Application/GeradorCodigoReservaTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using OnibusExpress.Application.Services;
+using Shouldly;
+using Xunit;
+
+namespace OnibusExpress.Tests.Application;
+
+public class GeradorCodigoReservaTests
+{
+    private readonly GeradorCodigoReserva _gerador = new();
+
+    [Fact]
+    public void Deve_Gerar_Codigo_No_Formato_Esperado()
+    {
+        var codigo = _gerador.Gerar();
+
+        Regex.IsMatch(codigo, @"^[A-Z]{3}-\d{5}$").ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Deve_Gerar_Codigos_Diferentes_Em_Chamadas_Sucessivas()
+    {
+        var codigos = Enumerable.Range(0, 100)
+            .Select(_ => _gerador.Gerar())
+            .ToList();
+
+        // não garante unicidade absoluta (isso é responsabilidade do banco/índice único),
+        // mas garante que o gerador não é determinístico/travado num valor fixo
+        codigos.Distinct().Count().ShouldBeGreaterThan(1);
+    }
+}
